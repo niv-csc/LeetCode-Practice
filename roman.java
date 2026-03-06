4fe89c55454edb7c9dd161ca3113687504aa9c86
𝐍𝐢𝐯𝐫𝐢𝐭𝐢 𝐌𝐮𝐭𝐡𝐮𝐯𝐚𝐢𝐫𝐚𝐯𝐚𝐧 <nivriti3266@gmail.com>
2026-03-06 15:43:31 +0400

    Create roman.java

---
+++ roman.java
@@ -0,0 +1,31 @@
+class roman{
+    public int romanToInt(String s) {
+        int result = 0;
+        int prev = 0;
+        
+        for (int i = s.length() - 1; i >= 0; i--) {
+            char c = s.charAt(i);
+            int curr = 0;
+            
+            switch (c) {
+                case 'I': curr = 1; break;
+                case 'V': curr = 5; break;
+                case 'X': curr = 10; break;
+                case 'L': curr = 50; break;
+                case 'C': curr = 100; break;
+                case 'D': curr = 500; break;
+                case 'M': curr = 1000; break;
+            }
+            
+            if (curr < prev) {
+                result -= curr;
+            } else {
+                result += curr;
+            }
+            
+            prev = curr;
+        }
+        
+        return result;
+    }
+}
